8ad0b4c6bed44d0972070249cc62751211e3705d
liweihan <weihanli@outlook.com>
2017-04-27 23:16:40 +0800

    补充提交数据库数据类型文件

---
+++ DbTool/DbType.cs
@@ -0,0 +1,41 @@
+﻿namespace DbTool
+{
+    /// <summary>
+    /// 数据库类型 
+    /// </summary>
+    public enum DbType
+    {
+        BigInt = 0,
+        Binary = 1,
+        Bit = 2,
+        Char = 3,
+        Date = 4,
+        DateTime = 5,
+        DateTime2 = 6,
+        DateTimeOffset = 7,
+        Decimal = 8,
+        Float = 9,
+        Image = 10,
+        Int = 11,
+        Money = 12,
+        NChar = 13,
+        NText = 14,
+        Numeric = 15,
+        NVarChar = 16,
+        Real = 17,
+        RowVersion = 18,
+        SmallDateTime = 19,
+        SmallInt = 20,
+        SmallMoney = 21,
+        Text = 22,
+        Time = 23,
+        Timestamp = 24,
+        TinyInt = 25,
+        UniqueIdentifier = 26,
+        VarBinary = 27,
+        VarChar = 28,
+        Variant = 29,
+        Xml = 30,
+        Structured = 31
+    }
+}
