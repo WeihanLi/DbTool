10d6d98bfadf245b0b9eff88233602558f3e345d
weihanli <weihanli@outlook.com>
2020-02-23 16:17:26 +0800

    add IServiceModule

---
+++ src/DbTool.Core/IServiceModule.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DbTool.Core
+{
+    public interface IServiceModule
+    {
+        void ConfigureServices(IServiceCollection services);
+    }
+}
