dd5e8a0a26c3046fe61a34087015e16a34d0241a
weihanli <weihanli@outlook.com>
2020-02-24 03:55:59 +0800

    separate IServiceColllection extension

--- DbTool/Extensions/DbProviderExtensions.cs
+++ DbTool/Extensions/DbProviderExtensions.cs
@@ -11,8 +11,6 @@ using DbTool.Core;
 using DbTool.Core.Entity;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using WeihanLi.Common;
 using WeihanLi.Common.Models;
 using WeihanLi.Extensions;
@@ -22,30 +20,6 @@ namespace DbTool
 {
     public static class DbProviderExtensions
     {
-        /// <summary>
-        /// Register TDbProvider service
-        /// </summary>
-        /// <typeparam name="TDbProvider">DbProvider type</typeparam>
-        /// <param name="serviceCollection">services</param>
-        /// <returns>services</returns>
-        public static IServiceCollection AddDbProvider<TDbProvider>(this IServiceCollection serviceCollection) where TDbProvider : IDbProvider
-        {
-            serviceCollection.TryAddEnumerable(new ServiceDescriptor(typeof(IDbProvider), typeof(TDbProvider), ServiceLifetime.Singleton));
-            return serviceCollection;
-        }
-
-        /// <summary>
-        /// Register TDbProvider service
-        /// </summary>
-        /// <typeparam name="TDbDocExporter">DbDocExporter type</typeparam>
-        /// <param name="serviceCollection">services</param>
-        /// <returns>services</returns>
-        public static IServiceCollection AddDbDocExporter<TDbDocExporter>(this IServiceCollection serviceCollection) where TDbDocExporter : IDbDocExporter
-        {
-            serviceCollection.TryAddEnumerable(new ServiceDescriptor(typeof(IDbDocExporter), typeof(TDbDocExporter), ServiceLifetime.Singleton));
-            return serviceCollection;
-        }
-
         /// <summary>
         /// 从 源代码 中获取表信息
         /// </summary>
---
+++ DbTool/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using DbTool.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace DbTool
+{
+    public static class DependencyInjectionExtension
+    {
+        /// <summary>
+        /// Register TDbProvider service
+        /// </summary>
+        /// <typeparam name="TDbProvider">DbProvider type</typeparam>
+        /// <param name="serviceCollection">services</param>
+        /// <returns>services</returns>
+        public static IServiceCollection AddDbProvider<TDbProvider>(this IServiceCollection serviceCollection) where TDbProvider : IDbProvider
+        {
+            serviceCollection.TryAddEnumerable(new ServiceDescriptor(typeof(IDbProvider), typeof(TDbProvider), ServiceLifetime.Singleton));
+            return serviceCollection;
+        }
+
+        /// <summary>
+        /// Register TDbProvider service
+        /// </summary>
+        /// <typeparam name="TDbDocExporter">DbDocExporter type</typeparam>
+        /// <param name="serviceCollection">services</param>
+        /// <returns>services</returns>
+        public static IServiceCollection AddDbDocExporter<TDbDocExporter>(this IServiceCollection serviceCollection) where TDbDocExporter : IDbDocExporter
+        {
+            serviceCollection.TryAddEnumerable(new ServiceDescriptor(typeof(IDbDocExporter), typeof(TDbDocExporter), ServiceLifetime.Singleton));
+            return serviceCollection;
+        }
+    }
+}
