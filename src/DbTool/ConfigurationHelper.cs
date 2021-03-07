8d6fbb103a131a08b556267be195bedfe4b19e09
weihanli <weihanli@outlook.com>
2021-03-07 17:33:48 +0800

    clean unused methods

--- src/DbTool/ConfigurationHelper.cs
+++ src/DbTool/ConfigurationHelper.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
 using System.Configuration;
 using WeihanLi.Extensions;
 
 namespace DbTool
 {
-#nullable disable
-
     /// <summary>
     /// Helper for ConfigurationManager
     /// https://github.com/WeihanLi/WeihanLi.Common/blob/dev/src/WeihanLi.Common/Helpers/ConfigurationHelper.cs
@@ -25,35 +22,29 @@ namespace DbTool
         /// </summary>
         /// <param name="key">设置的键值</param>
         /// <returns>键值对应的值</returns>
-        public static string AppSetting(string key) => _appSettings[key] ?? throw new ArgumentNullException(nameof(key));
+        public static string AppSetting(string key) => _appSettings[key] ?? string.Empty;
 
         /// <summary>
         /// 获取配置文件中AppSetting节点的值
         /// </summary>
         /// <param name="key">设置的键值</param>
         /// <returns>键值对应的值</returns>
-        public static T AppSetting<T>(string key) => _appSettings[key].StringToType<T>();
+        public static T AppSetting<T>(string key) => AppSetting(key).StringToType<T>();
 
-        public static T AppSetting<T>(string key, T defaultValue) => _appSettings[key].StringToType(defaultValue);
+        /// <summary>
+        /// 更新 AppSetting 配置
+        /// </summary>
+        /// <typeparam name="T">value type</typeparam>
+        /// <param name="key">app setting key</param>
+        /// <param name="value">app setting value</param>
+        public static void UpdateAppSetting<T>(string key, T value) => UpdateAppSetting(key, value.ToJsonOrString());
 
-        public static T AppSetting<T>(string key, Func<T> defaultValueFactory) => _appSettings[key].StringToType(defaultValueFactory());
-
-        public static bool AddAppSetting<T>(string key, T value) => AddAppSetting(key, value.ToJsonOrString());
-
-        public static bool AddAppSetting(string key, string value)
-        {
-            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            config.AppSettings.Settings.Add(key, value);
-            config.Save(ConfigurationSaveMode.Minimal);
-            // Force a reload of a changed section.
-            ConfigurationManager.RefreshSection("appSettings");
-            _appSettings = ConfigurationManager.AppSettings;
-            return true;
-        }
-
-        public static bool UpdateAppSetting<T>(string key, T value) => UpdateAppSetting(key, value.ToJsonOrString());
-
-        public static bool UpdateAppSetting(string key, string value)
+        /// <summary>
+        /// 更新 AppSetting 配置
+        /// </summary>
+        /// <param name="key">app setting key</param>
+        /// <param name="value">app setting value</param>
+        public static void UpdateAppSetting(string key, string value)
         {
             var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
             config.AppSettings.Settings.Remove(key);
@@ -62,16 +53,6 @@ namespace DbTool
             // Force a reload of a changed section.
             ConfigurationManager.RefreshSection("appSettings");
             _appSettings = ConfigurationManager.AppSettings;
-            return true;
         }
-
-        /// <summary>
-        /// 获取配置文件中ConnectionStrings节点的值
-        /// </summary>
-        /// <param name="key">键值</param>
-        /// <returns>键值对应的连接字符串值</returns>
-        public static string ConnectionString(string key) => ConfigurationManager.ConnectionStrings[key].ConnectionString;
     }
-
-#nullable restore
 }
